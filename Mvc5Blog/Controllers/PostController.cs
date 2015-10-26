cf22259d8d9878d227b558663dd1808680ce0ef4
knolegsb <seon2you@hotmail.com>
2015-10-25 23:58:14 -0700

    added database and model and postcontroller

---
+++ Mvc5Blog/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using Mvc5Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        private Mvc5BlogModel model = new Mvc5BlogModel();
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Update(int? id, string title, string body, DateTime dateTime, string tags)
+        {
+            if(!IsAdmin)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Post post = GetPost(id);
+
+            return null;
+        }
+
+        private Post GetPost(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAdmin { get { return true; /*return Session["IsAdmin"] != null && (bool)Session["IsAdmin"]; */} }
+    }
+}
